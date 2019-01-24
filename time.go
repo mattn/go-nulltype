b97dc3e8be50547df61095a1d5d1730a553ed2b5
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2019-01-24 10:00:35 +0900

    Add NullTime

---
+++ time.go
@@ -0,0 +1,88 @@
+package nulltype
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"time"
+)
+
+// NullTime is null friendly type for string.
+type NullTime struct {
+	t time.Time
+	v bool // Valid is true if Time is not NULL
+}
+
+// NullTimeOf return NullTime that he value is set.
+func NullTimeOf(value time.Time) NullTime {
+	var t NullTime
+	t.Set(value)
+	return t
+}
+
+// Valid return the value is valid. If true, it is not null value.
+func (t *NullTime) Valid() bool {
+	return t.v
+}
+
+// TimeValue return the value.
+func (t *NullTime) TimeValue() time.Time {
+	return t.t
+}
+
+// Reset set nil to the value.
+func (t *NullTime) Reset() {
+	t.t = time.Unix(0, 0)
+	t.v = false
+}
+
+// Set set the value.
+func (t *NullTime) Set(value time.Time) {
+	t.v = true
+	t.t = value
+}
+
+// Scan is a method for database/sql.
+func (t *NullTime) Scan(value interface{}) error {
+	t.t, t.v = value.(time.Time)
+	return nil
+}
+
+// Time return string indicated the value.
+func (t NullTime) String() string {
+	if !t.v {
+		return "nil"
+	}
+	return t.t.Format("2006/01/02 15:04:05")
+}
+
+// MarshalJSON encode the value to JSON.
+func (t NullTime) MarshalJSON() ([]byte, error) {
+	if !t.v {
+		return []byte("null"), nil
+	}
+	return json.Marshal(t.t.Format(time.RFC3339))
+}
+
+// UnmarshalJSON decode data to the value.
+func (t *NullTime) UnmarshalJSON(data []byte) error {
+	var value *string
+	if err := json.Unmarshal(data, &value); err != nil {
+		return err
+	}
+	t.v = value != nil
+	if value == nil {
+		t.t = time.Unix(0, 0)
+	} else {
+		tt, err := time.Parse(time.RFC3339, *value)
+		if err != nil {
+			return err
+		}
+		t.t = tt
+	}
+	return nil
+}
+
+// Value implement driver.Valuer.
+func (t NullTime) Value() (driver.Value, error) {
+	return t.t, nil
+}
---
+++ time_test.go
@@ -0,0 +1,134 @@
+package nulltype
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestNullTimeStringer(t *testing.T) {
+	var nt NullTime
+
+	want := "nil"
+	got := fmt.Sprint(nt)
+	if got != want {
+		t.Fatalf("want %v, but %v:", want, got)
+	}
+
+	now := time.Now()
+	want = now.Format("2006/01/02 15:04:05")
+	nt.Set(now)
+	got = fmt.Sprint(nt)
+	if got != want {
+		t.Fatalf("want %v, but %v:", want, got)
+	}
+
+	nt = NullTimeOf(now)
+	got = fmt.Sprint(nt)
+	if got != want {
+		t.Fatalf("want %v, but %v:", want, got)
+	}
+
+	want = "nil"
+	nt.Reset()
+	got = fmt.Sprint(nt)
+	if got != want {
+		t.Fatalf("want %v, but %v:", want, got)
+	}
+}
+
+func TestNullTimeMarshalJSON(t *testing.T) {
+	var nt NullTime
+
+	var buf bytes.Buffer
+	err := json.NewEncoder(&buf).Encode(nt)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := "null"
+	got := strings.TrimSpace(buf.String())
+	if got != want {
+		t.Fatalf("want %v, but %v:", want, got)
+	}
+
+	buf.Reset()
+
+	now := time.Now()
+	nt.Set(now)
+	err = json.NewEncoder(&buf).Encode(nt)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want = `"` + now.Format(time.RFC3339) + `"`
+	got = strings.TrimSpace(buf.String())
+	if got != want {
+		t.Fatalf("want %v, but %v:", want, got)
+	}
+}
+
+func TestNullTimeUnmarshalJSON(t *testing.T) {
+	var nt NullTime
+
+	err := json.NewDecoder(strings.NewReader("null")).Decode(&nt)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if nt.Valid() {
+		t.Fatalf("must be null but got %v", nt)
+	}
+
+	err = json.NewDecoder(strings.NewReader(`"2019-02-01T11:12:13Z"`)).Decode(&nt)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !nt.Valid() {
+		t.Fatalf("must not be null but got nil")
+	}
+
+	now, _ := time.Parse(time.RFC3339, "2019-02-01T11:12:13Z")
+	want := now
+	got := nt.TimeValue()
+	if got != want {
+		t.Fatalf("want %v, but %v:", want, got)
+	}
+
+	err = json.NewDecoder(strings.NewReader("{}")).Decode(&nt)
+	if err == nil {
+		t.Fatal("should be fail")
+	}
+}
+
+func TestNullTimeValueConverter(t *testing.T) {
+	var nt NullTime
+
+	err := nt.Scan(time.Now())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !nt.Valid() {
+		t.Fatalf("must not be null but got nil")
+	}
+
+	now := time.Now()
+	want := now
+	got := nt.TimeValue()
+	if got != want {
+		t.Fatalf("want %v, but %v:", want, got)
+	}
+
+	gotv, err := nt.Value()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if gotv != want {
+		t.Fatalf("want %v, but %v:", want, got)
+	}
+}
