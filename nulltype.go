8ec64c7b9ea7756670abf57c8441cae0c6aabaa6
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2019-01-31 10:15:03 +0900

    godoc

---
+++ nulltype.go
@@ -0,0 +1,7 @@
+// Package nulltype implements the null-able types
+// that is friendly to json.Encoder, json.Decoder, database/sql, fmt.Stringer,
+// text/template, html/template, some of ORMs. All of types provided have
+// methods that is provided from fmt.Stringer, json.Marshaler,
+// json.Unmarshaler, database/sql.Valuer.
+
+package nulltype
